4d17ea3d619d4259da1fb443aa62314fe26665e9
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-11-06 22:48:16 -0500

    [Completed] MVP completed, minor fixes

--- src/components/APODCard/Card.js
+++ src/components/APODCard/Card.js
@@ -3,10 +3,10 @@ import CardTitle from './CardTitle/CardTitle';
 import CardImage from './CardImage/CardImage';
 import CardInfo from  './CardInfo/CardInfo';
 const Card = props =>{
-     const {date, title, explanation, hdurl, url, } = props.data;
+    const {date, title, explanation, hdurl, url, } = props.data;
 
-     //loading card data
-     if(!hdurl) return <h3 className="loading">Loading...</h3>
+    //loading card data
+    if(!hdurl) return <h3 className="loading">Loading...</h3>
 
     return(
         <article className="card">
