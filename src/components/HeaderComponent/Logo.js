47ef4dd90d3d8d6923cec0538a44e6899ee5fb5f
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-11-06 22:37:53 -0500

    [Completed] MVP completed

--- src/components/HeaderComponent/Logo.js
+++ src/components/HeaderComponent/Logo.js
@@ -5,7 +5,7 @@ import nasaLogo from '../../imgs/nasa-logo.png'
 const Logo = () =>{
     return(
         <div className="logo-container">
-             <img className="header-logo" src={nasaLogo} alt="Nasa Logo" width='80%' height='40%'/>
+             <img className="header-logo" src={nasaLogo} alt="Nasa Logo" width='50%' height='10%'/>
        </div>
     )
 }
