11ca01ee6e38dfc0a0ad76cdaabc8569019329af
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-11-06 21:11:44 -0500

    [Add] Added axios call to useEffect, returned data from NASA API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import Header from './components/HeaderComponent/Header';
+
 
 function App() {
   const [apod, setApod] = useState([]);
 
-
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=BSWV1ZHzNOhKxaqol3ehAGQwcpuIQgNhseh9N4hJ')
+    .then(response => {
+      console.log(response.data);
+    })
+  }, []);
 
   return (
     <div className="App">
