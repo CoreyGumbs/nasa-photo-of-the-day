79ad13aa97e1827be6a62c8cf656d6876d4a1d26
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-11-06 21:16:20 -0500

    [Add] Created APOD card component, deleted other components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import Header from './components/HeaderComponent/Header';
+import Card from './components/APODCard/Card';
 
 
 function App() {
   const [apod, setApod] = useState([]);
 
-  useEffect(() => {
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=BSWV1ZHzNOhKxaqol3ehAGQwcpuIQgNhseh9N4hJ')
-    .then(response => {
-      console.log(response.data);
-    })
-  }, []);
+  // useEffect(() => {
+  //   axios.get('https://api.nasa.gov/planetary/apod?api_key=BSWV1ZHzNOhKxaqol3ehAGQwcpuIQgNhseh9N4hJ')
+  //   .then(response => {
+  //     console.log(response.data);
+  //   })
+  // }, []);
 
   return (
     <div className="App">
       <Header />  
+      <Card />
     </div>
   );
 }
---
+++ src/components/APODCard/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = () =>{
+    return(
+        <section className="card">
+            Card
+        </section>
+    )
+}
+
+export default Card;
