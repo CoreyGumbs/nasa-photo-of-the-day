37403cca23ec64564cd59875333b32bf0bf24f11
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-11-07 20:20:20 -0500

    [Add] Completed Card Title Component styling refactor and minor fixes

--- src/components/APODCard/CardTitle/CardTitle.js
+++ src/components/APODCard/CardTitle/CardTitle.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import moment from 'moment'
-import {CardTitleContainer, CardTitleH1} from '../StyledComponents/CardStyles';
+import {CardTitleContainer, CardTitleHeading, CardTitleSubHeading} from '../StyledComponents/CardStyles';
 
 
 const CardTitle = props => {
@@ -8,8 +8,8 @@ const CardTitle = props => {
     const dateTitle =  moment(date).format('dddd, MMMM Do YYYY');
     return(
         <CardTitleContainer>
-            <CardTitleH1>{title}</CardTitleH1>
-            <h2 className="card-title-date">{dateTitle}</h2>
+            <CardTitleHeading>{title}</CardTitleHeading>
+            <CardTitleSubHeading>{dateTitle}</CardTitleSubHeading>
         </CardTitleContainer>
     )
 }
--- src/components/APODCard/StyledComponents/CardStyles.js
+++ src/components/APODCard/StyledComponents/CardStyles.js
@@ -11,10 +11,15 @@ export const CardTitleContainer = styled.section`
     
 `;
 
-export const CardTitleH1 = styled.h1`
+export const CardTitleHeading = styled.h1`
     margin: 0 auto;
     padding: 20px 0;
     font-size: 5rem;
     color: #0B3C91;
     border-bottom: 1px solid black;
+`;
+
+export const CardTitleSubHeading = styled.h2`
+    font-size: 3rem;
+    color: #999;
 `;
